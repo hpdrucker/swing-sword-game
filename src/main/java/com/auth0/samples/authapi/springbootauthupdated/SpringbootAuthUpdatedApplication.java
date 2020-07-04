d7138ea3b0264054743263b409afae586d6d35d9
Luigi <lcavuoti@clip.ch>
2020-07-04 10:43:27 +0200

    finished example

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
@@ -1,5 +1,5 @@
 package com.auth0.samples.authapi.springbootauthupdated;
-
+// https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
